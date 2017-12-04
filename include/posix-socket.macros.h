d65f80689cbedfb6e4b702e0866eccc4cfa70151
Mikhail Vorozhtsov <mikhail.vorozhtsov@gmail.com>
2017-12-04 20:35:15 +0300

    Use hsc_printf

--- include/posix-socket.macros.h
+++ include/posix-socket.macros.h
@@ -3,12 +3,12 @@
 
 #ifndef hsc_alignment
 # define hsc_alignment(t) \
-   printf("%lu", (unsigned long) offsetof (struct { char x__; t (y__); }, y__));
+   hsc_printf("%lu", (unsigned long) offsetof (struct { char x__; t (y__); }, y__));
 #endif
 #define hsc_offsetof(s,f) \
-  printf("%lu", (unsigned long) offsetof (s, f));
+  hsc_printf("%lu", (unsigned long) offsetof (s, f));
 #define hsc_itype(t) \
-  printf("%s%lu", ((t)(-1)) < 0 ? "Int" : "Word", sizeof (t) * 8);
+  hsc_printf("%s%lu", ((t)(-1)) < 0 ? "Int" : "Word", sizeof (t) * 8);
 
 #endif /* POSIX_SOCKET_MARCROS_H */
 
